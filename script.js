0ead9b10a6bb9b575395bbbb5eb0f38976a634ce
Jean <jeanartcst@gmail.com>
2020-04-10 13:56:50 -0300

    Attached the Light/Dark mode on game.

--- script.js
+++ script.js
@@ -86,3 +86,48 @@ function iniciarJogo(){
 
 let jogo = setInterval(iniciarJogo, 100);
 
+
+
+/* Change navegator color */
+
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) => 
+    window
+            .getComputedStyle(element)
+            .getPropertyValue(style)
+
+          
+//catch bg style from css
+const initialColors = {                                     
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"), 
+}
+
+
+const darkMode = {                                     
+    bg: "#333333",
+    bgPanel: "#434343",
+    colorHeadings: "#F8F8FF",
+    colorText: "#b5b5b5"
+}
+
+
+const transformKey = key =>
+ "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key =>
+        html.style.setProperty(transformKey(key), colors[key])
+    )
+}
+
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
